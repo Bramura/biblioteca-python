74d785f37be3f9ecbcc03644216ca4aea567edc1
Bramura <miyamura.lagos@gmail.com>
2026-05-08 14:20:01 -0300

    feat: adiciona configuração inicial do sqlite

--- database.py
+++ database.py
@@ -1,7 +1,25 @@
 import sqlite3
 
+NOME_BANCO = "biblioteca.db"
+
 def conectar():
-	pass
+	conexao = sqlite3.connect(NOME_BANCO)
+	return conexao
+
 
 def criar_tabela():
-	pass
+	conexao = conectar()
+	cursor = conexao.cursor()
+	
+	cursor.execute("""
+	CREATE TABLE IF NOT EXISTS livros (
+        	id INTEGER PRIMARY KEY AUTOINCREMENT,
+        	titulo TEXT NOT NULL,
+        	autor TEXT NOT NULL,
+        	ano INTEGER,
+        	disponivel BOOLEAN NOT NULL DEFAULT 1
+    	)
+    	""")
+	
+	conexao.commit()
+	conexao.close()
---
+++ main.py
@@ -0,0 +1,5 @@
+from database import criar_tabela
+
+criar_tabela()
+
+print("Banco de dados iniciado com sucesso!")
